10b319a6c656bd6b6fa80c241c4eeda728971f6c
jahid <info.jahidulislamjahid@gmail.com>
2021-08-18 14:30:23 +0600

    phone price added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function updateQuantity(isIncreasing) {
-    const input = document.getElementById('case-input');
+function updateQuantity(product, price, isIncreasing) {
+    const input = document.getElementById(product + '-input');
     let inputText = input.value;
     if (isIncreasing) {
         inputText = parseInt(inputText) + 1;
@@ -9,22 +9,28 @@ function updateQuantity(isIncreasing) {
     }
     input.value = inputText;
     // updating the price 
-    const currentTotal = document.getElementById('case-price');
-    currentTotal.innerText = inputText * 59;
+    const currentTotal = document.getElementById(product + '-price');
+    currentTotal.innerText = inputText * price;
 }
 
-
-
+// phone event handler 
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateQuantity('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateQuantity('phone', 1219, false);
+});
+// case event handler 
 document.getElementById('case-plus').addEventListener('click', function () {
     /*  const input = document.getElementById('input-plus');
      const inputText = input.value;
      input.value = parseInt(inputText) + 1; */
-    updateQuantity(true);
+    updateQuantity('case', 59, true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function () {
     /*   const input = document.getElementById('input-plus');
       const inputText = input.value;
       input.value = parseInt(inputText) - 1; */
-    updateQuantity(false);
+    updateQuantity('case', 59, false);
 });
