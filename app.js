6ae8f4217103e0ddd982cf6575fe334687a9faa4
jahid <info.jahidulislamjahid@gmail.com>
2021-08-18 14:08:13 +0600

    calculation added.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('I am in a separate file')
+function updateQuantity(isIncreasing) {
+    const input = document.getElementById('case-input');
+    let inputText = input.value;
+    if (isIncreasing) {
+        inputText = parseInt(inputText) + 1;
+    }
+    else if (inputText > 0) {
+        inputText = parseInt(inputText) - 1;
+    }
+    input.value = inputText;
+    // updating the price 
+    const currentTotal = document.getElementById('case-price');
+    currentTotal.innerText = inputText * 59;
+}
+
+
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    /*  const input = document.getElementById('input-plus');
+     const inputText = input.value;
+     input.value = parseInt(inputText) + 1; */
+    updateQuantity(true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    /*   const input = document.getElementById('input-plus');
+      const inputText = input.value;
+      input.value = parseInt(inputText) - 1; */
+    updateQuantity(false);
+});
